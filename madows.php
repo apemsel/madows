af979218ebdfc0775c4e6eba75d951b716c7de1f
Adrian Pemsel <ap@jusmeum.de>
2013-07-10 19:59:49 +0200

    CLASSify ;-)

--- madows.php
+++ madows.php
@@ -4,80 +4,92 @@ namespace apemsel\madows;
 
 require("vendor/autoload.php");
 
-const CONFIG_FILE = "madows.json";
-const TEMPLATE_DIR = "templates";
 
-if (!$config = file_get_contents(CONFIG_FILE)) {
-  error("unable to read ".CONFIG_FILE);
-}
-
-$config = json_decode(file_get_contents("madows.json"), true);
-if (!$config) {
-  error("unable to parse ".CONFIG_FILE);
-}
-
-$parser_class = "dflydev\\markdown\\".$config["parser"];
-if (!class_exists($parser_class)) {
-  error("parser not found");
-}
-
-$parser = new $parser_class();
+$madows = new Madows();
+$madows->serve($_SERVER["REQUEST_URI"]);
 
 if (!isset($_SERVER["REQUEST_URI"])) {
-  error("REQUEST_URI not set");
-}
-$url_parts = parse_url($_SERVER['REQUEST_URI']);
-if (!$url_parts) {
-  error("unable to parse URI");
+  die("REQUEST_URI not set");
 }
 
-if (!isset($url_parts["path"]) or preg_match("/^\./", $url_parts["path"])) {
-  error("illegal path");
-}
-
-$markdown_file = basename($url_parts["path"]);
-if (!file_exists($markdown_file)) {
-  error("file not found: $markdown_file", 404);
-}
-
-$markdown = file_get_contents($markdown_file);
-if (!$markdown) {
-  error("unable to read: $requested");
-}
-
-$body = $parser->transformMarkdown($markdown);
-
-$template_file = TEMPLATE_DIR.DIRECTORY_SEPARATOR.$config["template"];
-if(!file_exists($template_file)) {
-  error("file not found: ".$template_file);
-}
-
-// Using a function to require() to have a clean context for the template
-$render = function($template_file, $context) {
-  require($template_file);
-};
-
-render($template_file, array(
-  "body" => $body,
-  "title" => $markdown_file
-));
-
-function render($template_file, $context) 
+class Madows
 {
-  extract($context, true);
-  require($template_file);
-};
+  const CONFIG_FILE = "madows.json";
+  const TEMPLATE_DIR = "templates";
 
-function error($message, $code = 500)
-{
-  @header("HTTP/1.1 $code");
-  @header("Content-type: text/html");
-  @header("Connection: Close");
-  ?><!DOCTYPE HTML>
-<html><head>
-<title><?php echo $code ?></title>
-</head><body>
-<h1><?php echo $code.' - '.$message ?></h1>
-</body></html><?php
-  die();
+  protected $config;
+  protected $parser;
+
+  public function __construct() 
+  {
+    if (!$config = file_get_contents(self::CONFIG_FILE)) {
+      error("unable to read ".self::CONFIG_FILE);
+    }
+    
+    $this->config = json_decode(file_get_contents("madows.json"), true);
+    if (!$this->config) {
+      error("unable to parse ".self::CONFIG_FILE);
+    }
+
+    $parser_class = "dflydev\\markdown\\".$this->config["parser"];
+    if (!class_exists($parser_class)) {
+      error("parser not found");
+    }
+
+    $this->parser = new $parser_class();
+  }
+
+  public function serve($request)
+  {
+    $url_parts = parse_url($request);
+    if (!$url_parts) {
+      error("unable to parse URI");
+    }
+    
+    if (!isset($url_parts["path"]) or preg_match("/^\./", $url_parts["path"])) {
+      error("illegal path");
+    }
+
+    $markdown_file = basename($url_parts["path"]);
+    if (!file_exists($markdown_file)) {
+      error("file not found: $markdown_file", 404);
+    }
+    
+    $markdown = file_get_contents($markdown_file);
+    if (!$markdown) {
+      error("unable to read: $requested");
+    }
+    
+    $body = $this->parser->transformMarkdown($markdown);
+
+    $template_file = self::TEMPLATE_DIR.DIRECTORY_SEPARATOR.$this->config["template"];
+    if(!file_exists($template_file)) {
+      error("file not found: ".$template_file);
+    }
+    
+    $this->render($template_file, array(
+      "body" => $body,
+      "title" => $markdown_file
+    ));
+  }
+  
+  function render($template_file, $context) 
+  {
+    extract($context, true);
+    require($template_file);
+  }
+  
+  function error($message, $code = 500)
+  {
+    @header("HTTP/1.1 $code");
+    @header("Content-type: text/html");
+    @header("Connection: Close");
+    ?><!DOCTYPE HTML>
+  <html><head>
+  <title><?php echo $code ?></title>
+  </head><body>
+  <h1><?php echo $code.' - '.$message ?></h1>
+  </body></html><?php
+    die();
+  }
 }
