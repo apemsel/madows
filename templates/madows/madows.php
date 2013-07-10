a950d77738809d4b263f18a3ddd20f32e6021220
Adrian Pemsel <ap@jusmeum.de>
2013-07-10 19:32:12 +0200

    basic template

--- templates/madows/madows.php
+++ templates/madows/madows.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="templates/madows/css/screen.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="templates/madows/css/print.css" type="text/css" media="print">
   </head>
   <body>
     <?php echo $body ?>
