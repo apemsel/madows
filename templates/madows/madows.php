b98b9d1d7288d9a0480f09eadc16db6c9f1c3d63
Adrian Pemsel <ap@jusmeum.de>
2013-07-11 21:56:55 +0200

    icons

--- templates/madows/madows.php
+++ templates/madows/madows.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <title><?php echo $title ?></title>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" href="templates/madows/css/screen.css" type="text/css" media="screen">
     <link rel="stylesheet" href="templates/madows/css/print.css" type="text/css" media="print">
     <script src="templates/madows/js/madows.js"></script>
@@ -9,9 +10,9 @@
   <body>
     <header>
       <nav>
-        <a href="/">Index</a>
-        <a id="toggle_toc" href="javascript:toggleDisplay('TOC')">Table of Contents</a>
-        <a href="<?php echo $_SERVER['REQUEST_URI'].'?source' ?>">Source</a>
+        <a href="/"><i class="icon-home"></i></a>
+        <a id="toggle_toc" href="javascript:toggleDisplay('TOC')"><i class="icon-list"></i></a>
+        <a href="<?php echo $_SERVER['REQUEST_URI'].'?source' ?>"><i class="icon-file-text"></i></a>
       </nav>
       <section id="TOC">
         <?php echo $toc ?>  
