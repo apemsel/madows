49ffc5f59a590770779e18d53af99c16f6eef998
Adrian Pemsel <ap@jusmeum.de>
2013-07-13 17:45:29 +0200

    template work

--- templates/madows/madows.php
+++ templates/madows/madows.php
@@ -3,15 +3,15 @@
   <head>
     <title><?php echo $title ?></title>
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="templates/madows/css/screen.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="templates/madows/css/screen.css" type="text/css" media="screen,print">
     <link rel="stylesheet" href="templates/madows/css/print.css" type="text/css" media="print">
     <script src="templates/madows/js/madows.js"></script>
   </head>
   <body>
     <nav>
-      <a href="/"><i class="icon-home" title="Home"></i></a>
-      <a id="toggle_toc" href="javascript:toggleTOC()"><i class="icon-list" title="Table of Contents"></i></a>
-      <a href="<?php echo $_SERVER['REQUEST_URI'].'?source' ?>"><i class="icon-file-text" title="Markdown"></i></a>
+      <a href="/"><i class="icon-home icon-fixed-width"></i><span>Home</span></a>
+      <a id="toggle_toc" href="javascript:toggleTOC()"><i class="icon-list icon-fixed-width"></i><span>TOC</span></a>
+      <a href="<?php echo $_SERVER['REQUEST_URI'].'?source' ?>"><i class="icon-file-text icon-fixed-width"></i><span>Markdown</span></a>
     </nav>
     <section class="page">
       <header>
